e783b45dc4e1d961b7d74a8e48e375f9e8948e15
polyglotDataNerd <gerardnino@gmail.com>
2020-08-13 08:42:42 -0700

    able to run Flink locally with flink-conf.yaml configurtaion

--- src/main/scala/com/poly/flink/FlinkLoader.scala
+++ src/main/scala/com/poly/flink/FlinkLoader.scala
@@ -4,7 +4,10 @@ import com.poly.flink.table.TableAPI
 import com.poly.utils.{ConfigProps, Utils}
 import org.apache.flink.api.common.operators.Order
 import org.apache.flink.api.scala._
-import org.apache.hadoop.conf.Configuration
+import org.apache.flink.configuration.GlobalConfiguration
+import org.apache.flink.core.fs.FileSystem
+import org.apache.flink.core.plugin.PluginUtils
+//import org.apache.flink.api.java.ExecutionEnvironment
 
 /**
  * Created by gbartolome on 08/05/2020.
@@ -19,32 +22,58 @@ object FlinkLoader extends java.io.Serializable {
   val utils: Utils = new Utils()
 
   def main(args: Array[String]): Unit = {
-    val config: Configuration = new Configuration()
-    config.set("hadoop.fs.s3a.access.key", utils.getSSMParam("/s3/polyglotDataNerd/admin/AccessKey"))
-    config.set("hadoop.fs.s3a.secret.key", utils.getSSMParam("/s3/polyglotDataNerd/admin/SecretKey"))
-    config.set("hadoop.fs.s3a.endpoint", "s3.us-west-2.amazonaws.com")
-    config.set("hadoop.fs.s3a.fast.upload", "true")
+    try {
+      val hadoopConfig: org.apache.hadoop.conf.Configuration =  new org.apache.hadoop.conf.Configuration()
+      hadoopConfig.set("fs.s3a.access.key", utils.getSSMParam("/s3/polyglotDataNerd/admin/AccessKey"))
+      hadoopConfig.set("fs.s3a.secret.key", utils.getSSMParam("/s3/polyglotDataNerd/admin/SecretKey"))
+      hadoopConfig.set("fs.s3a.endpoint", "s3.us-west-2.amazonaws.com")
+      hadoopConfig.set("fs.s3a.fast.upload", "true")
 
-    val benv = ExecutionEnvironment.createLocalEnvironment()
-    //runFlink(benv)
-    new TableAPI(benv, "s3a://poly-testing/covid/orc/combined/", config).batchORC()
+      val config: org.apache.flink.configuration.Configuration = new org.apache.flink.configuration.Configuration()
+      config.setString("s3a.access.key", utils.getSSMParam("/s3/polyglotDataNerd/admin/AccessKey"))
+      config.setString("s3a.secret.key", utils.getSSMParam("/s3/polyglotDataNerd/admin/SecretKey"))
+      config.setString("s3a.endpoint", "s3.us-west-2.amazonaws.com")
+      config.setString("s3a.fast.upload", "true")
+      /*
+      needs to set flink-conf.yaml with AWS keys to run local with ENV Variable
+      https://stackoverflow.com/questions/48460533/how-to-set-presto-s3-xxx-properties-when-running-flink-from-an-ide
+       */
+      FileSystem.initialize(GlobalConfiguration.loadConfiguration(System.getenv("FLINK_CONF_DIR")), PluginUtils.createPluginManagerFromRootFolder(config))
+      val benv: ExecutionEnvironment = ExecutionEnvironment.createLocalEnvironment(config)
+      //runFlink(benv)
+      new TableAPI(benv, "s3a://poly-testing/covid/orc/combined/", hadoopConfig).batchORC()
+      benv.executeAsync("app_test")
+    }
+    catch {
+      case e: Exception => {
+        println("Exception", e)
+        System.exit(1)
+      }
+    }
   }
 
   def runFlink(bEnv: ExecutionEnvironment): Unit = {
-    val input = bEnv
-      .readTextFile("s3://poly-testing/covid/combined/")
-      //\w+ matches one or more word characters
-      .flatMap(_.toLowerCase().replaceAll("\"", "").split("\\W+"))
-      .filter(_.nonEmpty)
-      .map(x => (x, 1))
-      .groupBy(0)
-      .sum("_2")
-      .sortPartition(1, Order.DESCENDING)
-      .first(10)
-    //input.writeAsCsv("s3://poly-testing/covid/flink/", "\n", "\t")
-    input.print()
-
-    bEnv.execute("Word Count")
+    try {
+      val input = bEnv
+        .readTextFile("s3a://poly-testing/covid/combined/")
+        //\w+ matches one or more word characters
+        .flatMap(_.toLowerCase().replaceAll("\"", "").split("\\W+"))
+        .filter(_.nonEmpty)
+        .map(x => (x, 1))
+        .groupBy(0)
+        .sum("_2")
+        .sortPartition(1, Order.DESCENDING)
+        .first(10)
+      //input.writeAsCsv("s3://poly-testing/covid/flink/", "\n", "\t")
+      input.print()
+      bEnv.execute("Word Count")
+    }
+    catch {
+      case e: Exception => {
+        println("Exception", e)
+        System.exit(1)
+      }
+    }
 
   }
 }
--- src/main/scala/com/poly/flink/table/TableAPI.scala
+++ src/main/scala/com/poly/flink/table/TableAPI.scala
@@ -3,8 +3,8 @@ package com.poly.flink.table
 import org.apache.flink.api.scala.ExecutionEnvironment
 import org.apache.flink.orc.OrcTableSource
 import org.apache.flink.table.api.Table
-import org.apache.flink.table.api.bridge.scala.BatchTableEnvironment
 import org.apache.hadoop.conf.Configuration
+import org.apache.flink.table.api.scala.BatchTableEnvironment
 
 class TableAPI(env: ExecutionEnvironment, source: String, conf: Configuration) {
 
@@ -13,13 +13,15 @@ class TableAPI(env: ExecutionEnvironment, source: String, conf: Configuration) {
     val orc = OrcTableSource
       .builder()
       .path(source, true)
+      //.forOrcSchema("")
       .withConfiguration(conf)
       .build()
 
     tableEnv.registerTableSource("orcTable", orc)
+
     val orcTable: Table = tableEnv.sqlQuery(s"select * from orcTable limit 10")
     orc.getDataSet(env.asInstanceOf[org.apache.flink.api.java.ExecutionEnvironment]).print()
-    env.execute()
+    tableEnv.execute("test")
   }
 
 
